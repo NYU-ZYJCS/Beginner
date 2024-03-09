e240caab4aa47c0abed3a31a384795857519c593
nyuZyjCs <qq1219637652@gmail.com>
2024-03-08 19:59:20 -0500

    Initial commit

---
+++ src/main/java/com/example/beginner/BeginnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.beginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeginnerApplication.class, args);
+	}
+
+}
